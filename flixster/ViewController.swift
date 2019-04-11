a077e36f57be462da32b3d9705cf15507a62829f
Joey Hwang <hwangjt@uci.edu>
2019-04-11 11:12:02 -0700

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Joey Hwang on 4/11/19.
+//  Copyright © 2019 Joey Hwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
